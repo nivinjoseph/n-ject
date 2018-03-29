bb6586e06ab43f1c50f29e52aea0281183a7107f
nivinjoseph <nivinjoseph@outlook.com>
2018-03-29 04:15:27 -0400

    safari refactoring

--- src/component-registration.ts
+++ src/component-registration.ts
@@ -25,7 +25,7 @@ export class ComponentRegistration
         given(component, "component").ensureHasValue();
         given(lifestyle, "lifestyle").ensureHasValue();
         
-        (<any>this)[keySymbol] = key;
+        (<any>this)[keySymbol] = key.trim().split("").join(""); // Safari de-optimization
         this._component = component;
         this._lifestyle = lifestyle;
         this._dependencies = this.getDependencies();
